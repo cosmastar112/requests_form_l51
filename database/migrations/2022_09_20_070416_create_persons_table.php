18153eea2cc824ff88039eb25fb38ce6d3c4f393
cosmastar112 <cosmastar112@gmail.com>
2022-09-20 11:31:46 +0400

    миграции таблиц: пользователи и заявки

---
+++ database/migrations/2022_09_20_070416_create_persons_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePersonsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('persons', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('login');
+            $table->string('email');
+            $table->timestamps();
+
+            $table->unique('login');
+            $table->unique('email');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('persons', function (Blueprint $table) {
+            $table->dropIndex('login');
+            $table->dropIndex('email');
+        });
+        Schema::drop('persons');
+    }
+}
---
+++ database/migrations/2022_09_20_070434_create_requests_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRequestsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('requests', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('per_id');
+            $table->string('from');
+            $table->text('body');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('requests');
+    }
+}
